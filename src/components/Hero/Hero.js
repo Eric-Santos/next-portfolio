1c1600cb6948fd1b3ced760372d7aa3d1c49e5a6
Eric Santos <eric355402@yahoo.com>
2022-02-02 17:30:14 -0700

    hero section completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+	Section,
+	SectionText,
+	SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+	<Section row nopadding>
+		<LeftSection>
+			<SectionTitle main center>
+				Hello There! <br /> I am Eric Santos
+			</SectionTitle>
+			<SectionText>
+				Full Stack Developer focusing on Front-End Development
+			</SectionText>
+		</LeftSection>
+		<Button
+			onClick={() => (window.location = 'mailto:eric.santos.dev@gmail.com')}
+		>
+			Learn More
+		</Button>
+	</Section>
 );
 
 export default Hero;
