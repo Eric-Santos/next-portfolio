15d5df07e60e2ea1720dcf16496bf6371a40fb09
Eric Santos <eric355402@yahoo.com>
2022-02-12 15:09:27 -0700

    updated projects section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,42 +1,31 @@
 export const projects = [
 	{
-		title: 'MERN Memories',
+		title: 'React Slider',
 		description:
-			"Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-		image: '/images/1.png',
-		tags: ['Mongo', 'Express', 'React', 'Node'],
-		source: 'https://google.com',
-		visit: 'https://google.com',
+			'I built this image slider to replace previous sliders I have used in clients websites.',
+		image: '/images/react-slider.png',
+		tags: ['React', 'Progressive Web App', 'Framer-Motion'],
+		source: 'https://react-slider-orcin.vercel.app/',
+		visit: 'https://github.com/Eric-Santos/react-slider/tree/main',
 		id: 0,
+	},
+	{
+		title: 'Potential Adventure',
+		description:
+			'This is a project to create a responsive image slider and use styled components with React Router.',
+		image: '/images/potentialAdventure.png',
+		tags: ['React', 'React Router', 'Styled Components', 'Progressive Web App'],
+		source: 'https://cranky-agnesi-2be1a2.netlify.app/',
+		visit: 'https://github.com/eric-santos/potential-adventure',
+		id: 2,
 	},
 	{
 		title: 'E-Commerce',
 		description:
-			"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-		image: '/images/2.png',
-		tags: ['React', 'JavaScript'],
-		source: 'https://google.com',
-		visit: 'https://google.com',
-		id: 1,
-	},
-	{
-		title: 'WebRTC App',
-		description:
-			"This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-		image: '/images/3.jpg',
-		tags: ['React', 'WebRTC'],
-		source: 'https://google.com',
-		visit: 'https://google.com',
-		id: 2,
-	},
-	{
-		title: 'Unichat',
-		description:
-			'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
-		image: '/images/4.jpg',
-		tags: ['React', 'ChatEngine', 'Firebase'],
-		source: 'https://google.com',
-		visit: 'https://google.com',
+			'This is a code repository for a webshop meant to be used with a local restaurant ordering system. This is a work in progress as of Feb. 2022',
+		image: '/images/5.jpg',
+		tags: ['Next.js', 'Styled Components'],
+		visit: 'https://github.com/Eric-Santos/ecommerce',
 		id: 3,
 	},
 ];
